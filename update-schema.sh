bf674e12843a4ad18dcb19f38be115e917141a4c
Thomas Liske <thomas@fiasko-nw.net>
2020-06-09 17:52:38 +0200

    Update schema filename and front page.

--- update-schema.sh
+++ update-schema.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git checkout master -- schema/ifstate.conf.json
-generate-schema-doc schema/ifstate.conf.json schema/index.html
+git checkout master -- schema/ifstate.conf.schema.json
+generate-schema-doc schema/ifstate.conf.schema.json schema/index.html
