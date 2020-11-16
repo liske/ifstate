3869203ffa851c2d67ea1c8af5ac59218f129f60
Thomas Liske <thomas@fiasko-nw.net>
2020-11-17 00:36:31 +0100

    Finalize veth support.

---
+++ libifstate/link/veth.py
@@ -0,0 +1,20 @@
+from libifstate.util import logger, ipr
+from libifstate.link.base import Link
+from libifstate.exception import LinkCannotAdd
+
+class VethLink(Link):
+    def __init__(self, name, link, ethtool):
+        super().__init__(name, link, ethtool)
+
+    # quirk to handle peer attribute
+    def get_if_attr(self, key):
+        if key != "peer":
+            return super().get_if_attr(key)
+
+        peer = super().get_if_attr("link")
+
+        if peer is None:
+            return None
+
+        lnk = next(iter(ipr.get_links(peer)), None)
+        return lnk.get_attr("IFLA_IFNAME")
