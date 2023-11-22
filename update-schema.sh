fc1ce08cb86a4187312ab659c3ec309a9e7bf805
Thomas Liske <thomas@fiasko-nw.net>
2023-11-22 21:40:58 +0100

    schema: move generator config into file and improve settings

--- update-schema.sh
+++ update-schema.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 git checkout master -- schema/ifstate.conf.schema.json
-generate-schema-doc --no-link-to-reused-ref --expand-buttons --config template_name=js_offline schema/ifstate.conf.schema.json schema/index.html
+generate-schema-doc --config-file schema/config.yaml schema/ifstate.conf.schema.json schema/index.html
