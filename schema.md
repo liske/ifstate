4efa8dd07321f6cd02b6b3de2d734bb3ecc4c727
Thomas Liske <thomas@fiasko-nw.net>
2024-05-12 11:22:17 +0200

    schema: add support for version schema files and descriptions

--- schema.md
+++ schema.md
@@ -7,5 +7,5 @@ The `ifstatecli` command uses a config file based on *Yaml* and must comply to
 the provided *JSON Schema*. It is possible to include values from external files
 (i.e. private keys) using the provided `!include` tag.
 
-- [schema description](schema/)
-- [ifstate.conf.schema.json](schema/ifstate.conf.schema.json)
+- [schema description](schema/1/)
+- [ifstate.conf.schema.json](schema/1/ifstate.conf.schema.json)
--- update.sh
+++ update.sh
@@ -1,14 +1,24 @@
-#!/bin/sh
+#!/bin/sh -e
 
-# This script fetches to most recent schema and changelog from the master branch.
+# This script fetches schemas and changelog from the master branch.
 
-echo "Updating schema..."
-git checkout master -- schema/ifstate.conf.schema.json
-generate-schema-doc --config-file schema/config.yaml schema/ifstate.conf.schema.json schema/index.html
+release_tag=$(git tag -l "*.*.*"|sort -n|tail -n 1)
+version_dir=$(echo "$release_tag"|cut -d. -f1,2)
+major_dir=$(echo "$release_tag"|cut -d. -f1)
 
+echo "Updating schema for $release_tag..."
+mkdir -p "schema/$version_dir"
+rm -f schema/ifstate.conf.schema.json "schema/$version_dir/ifstate.conf.schema.json"
+git checkout "tags/$release_tag" -- schema/ifstate.conf.schema.json
+git mv schema/ifstate.conf.schema.json "schema/$version_dir/ifstate.conf.schema.json"
+generate-schema-doc --config-file schema/config.yaml "schema/$version_dir/ifstate.conf.schema.json" "schema/$version_dir/index.html"
+ln -vfns "$version_dir" "schema/$major_dir"
+
+echo "Restore legacy schema file..."
+cp schema/1/ifstate.conf.schema.json schema/ifstate.conf.schema.json
 
 echo "Updating changelog..."
-git checkout master -- CHANGELOG.md
+git checkout "tags/$release_tag" -- CHANGELOG.md
 cat <<EOF > changelog.md
 ---
 title: ChangeLog
