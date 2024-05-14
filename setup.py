f80a5eefd03a02548b89dc8e864781ad8dd46eb4
Thomas Liske <thomas@fiasko-nw.net>
2024-05-14 21:32:40 +0200

    schema: cleanup schema handling for 2.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     license="GPL3+",
     packages=find_packages(),
     package_data={
-        "libifstate": ["../schema/*/*.schema.json"],
+        "libifstate": ["../schema/2/ifstate.conf.schema.json"],
     },
     install_requires=install_requires(),
     extras_require={
