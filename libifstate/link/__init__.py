519895772e925f36e3526b4a4fba845b57e88d4d
Thomas Liske <thomas@fiasko-nw.net>
2024-09-03 11:12:33 +0200

    link: add a custom class for handling DSA links like physical links

--- libifstate/link/__init__.py
+++ libifstate/link/__init__.py
@@ -1,4 +1,5 @@
 import libifstate.link.base
+import libifstate.link.dsa
 import libifstate.link.physical
 import libifstate.link.tun
 import libifstate.link.veth
---
+++ libifstate/link/dsa.py
@@ -0,0 +1,10 @@
+from libifstate.util import logger
+from libifstate.link.physical import PhysicalLink
+
+class DsaLink(PhysicalLink):
+    """
+    Distributed Switch Architecture (DSA) user interface
+    
+    https://docs.kernel.org/networking/dsa/configuration.html
+    """
+    pass
